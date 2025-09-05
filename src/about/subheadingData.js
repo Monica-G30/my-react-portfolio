0b9c75ef4e5287347bad6a8fa3251e044cfea55a
Monica-G30 <wangui16monica@gmail.com>
2025-09-05 14:16:47 +0300

    Created the about page

---
+++ src/about/subheadingData.js
@@ -0,0 +1,94 @@
+const subheadingData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Hi, I’m Monica Gikaru! I recently completed the SheCodes Bootcamp, I
+            enjoy bringing designs to life with HTML, CSS, and JavaScript, and
+            I’m always curious to learn new tools that make the web more
+            creative and accessible. Right now, I’m excited to take the next
+            step in my journey by joining a team where I can keep growing as a
+            developer and contribute to meaningful projects.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Shecodes Bootcamp 2024-2025",
+      content: (
+        <>
+          <p>
+            At SheCodes, I learned how to build responsive websites using HTML,
+            CSS, and JavaScript, while gaining hands-on experience through real
+            projects that fueled my passion for tech.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Diploma in Medical Engineering 2021-2023",
+      content: (
+        <>
+          <p>
+            I hold a Diploma in Medical Engineering, where I gained hands-on
+            experience working with medical equipment, troubleshooting technical
+            issues, and ensuring reliable healthcare technology.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "Sales Rep Mejagron (Feb 2023- Sept 2024)",
+      content: (
+        <>
+          <p>
+            <li>Identify and Reach Out to Customers</li>
+            <li>Promote and Sell Products/Services</li>
+            <li>Negotiate and Close Sales</li>
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Biomed Intern Mbagathi Hospital  (Sep-Nov 2022)",
+      content: (
+        <>
+          <p>
+            <li>
+              Assisted in the installation, calibration and testing of
+              biomedical devices.
+            </li>
+            <li>
+              Performed preventive and corrective maintainance under
+              supervision.
+            </li>
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Biomed Intern Thika Level 5  Hospital (Jan-Mar 2020)",
+      content: (
+        <>
+          <p>
+            <li>
+              Help train healthcare staff on the usage of biomedical equipment.
+            </li>
+            <li>
+              Help troubleshoot equipment such as ECGs, ventilators and
+              analyzers.
+            </li>
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingData;
