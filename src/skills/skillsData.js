15a68994d952504e2ba34df339cad3fa54c8c6da
Monica-G30 <wangui16monica@gmail.com>
2025-09-16 13:08:51 +0300

    Created my skills page using SkillsMenu.js

---
+++ src/skills/skillsData.js
@@ -0,0 +1,56 @@
+const skills = {
+  1: [
+    {
+      title: "HTML",
+      level: 6,
+    },
+    {
+      title: "CSS",
+      level: 5,
+    },
+    {
+      title: "JavaScript",
+      level: 4,
+    },
+    {
+      title: "Tailwind Css",
+      level: 1,
+    },
+    {
+      title: "React",
+      level: 4,
+    },
+    {
+      title: "Figma",
+      level: 3,
+    },
+  ],
+  2: [
+    {
+      title: "postgreSQL",
+      level: 2,
+    },
+    {
+      title: "Node.js",
+      level: 3,
+    },
+    {
+      title: "Git",
+      level: 4,
+    },
+    {
+      title: "PHP",
+      level: 1,
+    },
+    {
+      title: "SQL",
+      level: 2,
+    },
+    {
+      title: "MangoDb",
+      level: 1,
+    },
+  ],
+};
+
+export default skills;
